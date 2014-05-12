7ec4e75b66d27a6c0bf139a7b74ccef8a5f90013
Zach Wood-Doughty <zach@eduroam-175-4.dyn.carleton.edu>
2014-05-12 02:26:12 -0500

    1st js

--- 1.php
+++ 1.php
@@ -5,6 +5,17 @@
 </head>
 
 <body>
+
+<button type="button" onclick="myFunction()">Date</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+    document.getElementById("demo").innerHTML = Date();
+}
+</script>
+
 <script type="text/javascript">
     var startTime = new Date();        //Start the clock!
     window.onbeforeunload = function()        //When the user leaves the page(closes the window/tab, clicks a link)...
