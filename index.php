e4c684888c5183955aac6b2d7b44c0d64adc826e
Zach Wood-Doughty <zach@eduroam-175-4.dyn.carleton.edu>
2014-05-12 11:55:16 -0500

    post form

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 <title>Mermoose</title>
 </head>
 
+<script>
+
+</script>
+
 <body>
 
 Welcome to Mermoose! This is a site with some words that you are going to read and while you do we will track how long it takes you to read them!
