4c455ef0cd229d6916d68e5d400297bd9683acbc
Zach Wood-Doughty <zach@eduroam-175-4.dyn.carleton.edu>
2014-05-12 02:32:55 -0500

    timer attempt

--- 1.php
+++ 1.php
@@ -6,16 +6,6 @@
 
 <body>
 
-<button type="button" onclick="myFunction()">Date</button>
-
-<p id="demo"></p>
-
-<script>
-function myFunction() {
-    document.getElementById("demo").innerHTML = Date();
-}
-</script>
-
 <script type="text/javascript">
     var startTime = new Date();        //Start the clock!
     window.onbeforeunload = function()        //When the user leaves the page(closes the window/tab, clicks a link)...
