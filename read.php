488d28ac20cdbd14b9b2d1cb8017c3528c3a5c27
zachwooddoughty <zachwooddoughty@gmail.com>
2014-05-12 16:41:25 -0500

    read basics

--- read.php
+++ read.php
@@ -2,30 +2,21 @@
 
 <head>
 <title>Mermoose</title>
-<script type="text/javascript" src="jquery-1.4.2.min.js"></script>
+<script>
+function leave()
+{
+$("#testForm").submit();
+alert("Thank you for visiting Mermoose!");
+}
+</script>
 </head>
 
-<body>
-<form name="myForm" id="testForm" method="POST" action="index.php">
+<body onunload="leave()">
+<!--<form name="myForm" id="testForm" method="POST" action="index.php">
 UserName: <input type="text" name="user" value="test" /> <br/>
 Password: <input type="password" name="password" value="test"/> <br/>
 </form>
-
-<button id="reload">Refresh a Page in jQuery</button>
- 
-<script type="text/javascript">
- 
-	$('#reload').click(function() {
- 
-	 	location.reload();
- 
-	});
- 
-	$(window).bind('beforeunload', function(){
-		alert("goodbye!");
-	});
- 
-</script>
+-->
 
 Woo this is an awesome article.
 
