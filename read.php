ee782cc3603bdc8b790ae07fe515cefa430bfb35
Zach Wood-Doughty <zach@eduroam-175-4.dyn.carleton.edu>
2014-05-12 14:56:02 -0500

    read basics

--- read.php
+++ read.php
@@ -2,19 +2,31 @@
 
 <head>
 <title>Mermoose</title>
+<script type="text/javascript" src="jquery-1.4.2.min.js"></script>
 </head>
-<script>
-$(window).unload(function () {
-    $("#testForm").submit();
-    alert("unloading!");
-});
 
-</script>
-</head>
+<body>
 <form name="myForm" id="testForm" method="POST" action="index.php">
 UserName: <input type="text" name="user" value="test" /> <br/>
 Password: <input type="password" name="password" value="test"/> <br/>
 </form>
+
+<button id="reload">Refresh a Page in jQuery</button>
+ 
+<script type="text/javascript">
+ 
+	$('#reload').click(function() {
+ 
+	 	location.reload();
+ 
+	});
+ 
+	$(window).bind('beforeunload', function(){
+		alert("goodbye!");
+	});
+ 
+</script>
+
 Woo this is an awesome article.
 
 <a href="index.php">Back to home</a>
