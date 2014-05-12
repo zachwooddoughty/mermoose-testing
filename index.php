6cccc76356f02978ed5473eb8c8369441cdcb88f
Zach Wood-Doughty <zach@eduroam-175-4.dyn.carleton.edu>
2014-05-12 14:47:59 -0500

    read basics

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
+<?php
+$name = $_POST['user'];
+?>
 <html>
-
 <head>
 <title>Mermoose</title>
 </head>
@@ -13,10 +15,7 @@
 Welcome to Mermoose! This is a site with some words that you are going to read and while you do we will track how long it takes you to read them!
 <br>
 <a href="read.php">A cool article</a>
-<?php
-$name = $_POST['userName']
-echo($name);
-?>
+I think that <?php echo "your name is" . $name ?> or something.
 
 </body>
 </html>
