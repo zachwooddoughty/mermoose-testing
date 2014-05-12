7e3a3097b73c4f2d295a5b43d6e31bded7f15f65
Zach Wood-Doughty <zach@eduroam-175-4.dyn.carleton.edu>
2014-05-12 14:44:50 -0500

    read basics

--- index.php
+++ index.php
@@ -13,6 +13,10 @@
 Welcome to Mermoose! This is a site with some words that you are going to read and while you do we will track how long it takes you to read them!
 <br>
 <a href="read.php">A cool article</a>
+<?php
+$name = $_POST['userName']
+echo($name);
+?>
 
 </body>
 </html>
