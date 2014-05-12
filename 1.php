554a7cc2a788b33e64160646992a21f92fae8bea
Zach Wood-Doughty <zach@eduroam-175-4.dyn.carleton.edu>
2014-05-12 02:24:47 -0500

    javascript test

--- 1.php
+++ 1.php
@@ -5,6 +5,15 @@
 </head>
 
 <body>
+<script type="text/javascript">
+    var startTime = new Date();        //Start the clock!
+    window.onbeforeunload = function()        //When the user leaves the page(closes the window/tab, clicks a link)...
+    {
+        var endTime = new Date();        //Get the current time.
+        var timeSpent=(endTime - startTime);        //Find out how long it's been.
+        alert(timeSpent);        //Pop up a window with the time spent in microseconds.
+    }
+</script>
 
 Check out this fucking awesome article!
 
