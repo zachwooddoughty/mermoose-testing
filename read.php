e400ebfb9c62757392b50a16cb420bf2a1c56305
zachwooddoughty <zachwooddoughty@gmail.com>
2014-05-12 23:21:19 -0500

    read basics

--- read.php
+++ read.php
@@ -10,16 +10,20 @@ function leave()
     var str2 = " seconds on this page!";
     var time = new Date().getTime() - start_time;
     alert(str1.concat(time, str2));
-    $("#testForm").submit();
+
+    document.myForm.time_input.value = '100';
+    document.myForm.submit();
+
+    //document.getElementById("time_input").value = time;
+    //document.getElementById("testForm").submit();
     
-    //document.getElementById("time_input").
 }
 </script>
 </head>
 
 <body onbeforeunload="leave()">
 <form name="myForm" id="testForm" method="POST" action="index.php">
-    <input type="text" name="time_input"/>
+    <input type="text" name="time_input" id="time_input"/>
 </form>
 
 Woo this is an awesome article.
